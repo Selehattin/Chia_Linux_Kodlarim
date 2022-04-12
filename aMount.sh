f210e63e103618af0d08d14624bd4e2c56c73d54
Selehattin <32058059+Selehattin@users.noreply.github.com>
2022-04-12 11:51:53 +0300

    Update aMount.sh

--- aMount.sh
+++ aMount.sh
@@ -1,6 +1,6 @@
 echo "FuserMount yapılıyor ..."
 sleep 3
-fusermount -u ~/hr05
+fusermount -u ~/gr01
 echo "Mount yapılıyor ..."
-rclone mount H5: /root/hr05 --allow-non-empty --daemon --vfs-cache-mode off --multi-thread-streams 30 --low-level-retries 2 --retries 2 --vfs-read-chunk-size 128K --drive-chunk-size 1M --buffer-size off --max-backlog 20000 --contimeout 9s --no-traverse --no-modtime --read-only --log-level INFO --stats 1m --drive-service-account-file /root/acc/selo01.json
+rclone mount G1: /root/gr01 --allow-non-empty --daemon --vfs-cache-mode off --multi-thread-streams 30 --low-level-retries 2 --retries 2 --vfs-read-chunk-size 128K --drive-chunk-size 1M --buffer-size off --max-backlog 20000 --contimeout 9s --no-traverse --no-modtime --read-only --log-level INFO --stats 1m --drive-service-account-file /root/add/selo01.json
 sleep 3
