940a5172cca8912b6e13af86fda5ff8bbf404de4
Selehattin <32058059+Selehattin@users.noreply.github.com>
2022-04-21 11:41:23 +0300

    Create bmnt.sh

---
+++ bmnt.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+index=0
+while :
+do
+index=$(($index + 1))
+PLOT_FOLDER="/root/ap0$index/"
+PLOT_COUNT=$(find "$PLOT_FOLDER"  | wc -l)
+MIN_PLOT_COUNT=1
+if [ $PLOT_COUNT -gt $MIN_PLOT_COUNT ] 
+then
+        echo "Devam Ediyor $index . "
+sleep 2
+else
+echo "Devam ......................"
+bash kur.sh
+bash amount.sh
+fi
+
+if [[ $index -gt 3 ]]; then
+      index=0
+fi
+done
