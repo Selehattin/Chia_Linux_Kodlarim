7ad85159bfc406b28583e66ca33a08528136cdd4
Selehattin <32058059+Selehattin@users.noreply.github.com>
2022-04-25 16:27:55 +0300

    Add files via upload

---
+++ m/cmount4.sh
@@ -0,0 +1,2 @@
+fusermount -l ~/cr04
+rclone mount C4: /root/cr04 --allow-non-empty --daemon --vfs-cache-mode off --multi-thread-streams 30 --low-level-retries 2 --retries 2 --vfs-read-chunk-size 128K --drive-chunk-size 1M --buffer-size off --max-backlog 20000 --contimeout 9s --no-traverse --no-modtime --read-only --log-level INFO --stats 1m --drive-service-account-file /root/acc/selo01.json
