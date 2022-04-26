f3db54099bcdec68486c64346f014a6d005b9985
Selehattin <32058059+Selehattin@users.noreply.github.com>
2022-04-26 04:54:33 +0300

    Update harvester.sh

--- harvester.sh
+++ harvester.sh
@@ -3,7 +3,7 @@ cd chia-blockchain
 sh install.sh
 . ./activate
 chia init -c /root/ca/
-chia configure --set-farmer-peer 5.161.65.15:8447
+chia configure --set-farmer-peer 5.161.46.169:8447
 chia plots add -d /root/depo1/pcm1
 chia configure --set-log-level INFO
 chia start harvester
