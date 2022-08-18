5b6f2b9a3fd01043103906233a6f0bc48c575b7e
Selehattin <32058059+Selehattin@users.noreply.github.com>
2022-08-19 02:45:17 +0300

    dd
    njjjd

---
+++ docker.sh
@@ -0,0 +1,18 @@
+ sudo killall apt apt-get
+ sudo rm /var/lib/apt/lists/lock
+ sudo rm /var/cache/apt/archives/lock
+ sudo rm /var/lib/dpkg/lock*
+ sudo dpkg --configure -a
+ sudo apt-get update -y;
+ sudo apt-get install -y ca-certificates;
+ sudo apt-get install -y curl;
+ sudo apt-get install -y gnupg;
+ sudo apt-get install -y lsb-release;
+ sudo mkdir -p /etc/apt/keyrings;
+ sudo rm /etc/apt/keyrings/*;
+ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg;
+ echo   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null;
+ sudo apt-get install -y docker-ce;
+ sudo apt-get install -y docker-ce-cli;
+ sudo apt-get install -y containerd.io;
+ sudo apt-get install -y docker-compose-plugin
