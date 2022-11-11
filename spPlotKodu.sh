4bc1b9dd650f4bbd10910a5dddd7ea230af9dbab
Selehattin <32058059+Selehattin@users.noreply.github.com>
2022-11-11 21:52:55 +0300

    Add files via upload

---
+++ spPlotKodu.sh
@@ -0,0 +1,2 @@
+cd chia-plotter
+screen -S spaceplot ./build/chia_plot -n 10000 -r 24 -u 256 -d /root/disk1/data/ -2 /root/ram/ -t  /root/disk1/temp/ -c xch1j87jt0uk2p79g7v8pf5ewyxuh0hrzxkxu6rqqg9zzcn3wamjdrxq5sxtve -f 88fe767ffb45d152457a4f6deeffc75cc885bc041a7215a51906261e56680724c8dc878772c1ea4aa60644e2aec0ac97  
