d6dd65c15726e320035430c5cd99e603deb16569
Selehattin <32058059+Selehattin@users.noreply.github.com>
2022-11-11 20:09:55 +0300

    Update chSpace.sh

--- chSpace.sh
+++ chSpace.sh
@@ -58,4 +58,6 @@ git submodule update --init;
 ./make_devel.sh;
 cd
 cd chia-plotter
+ip=$(curl ifconfig.me)
+telegram-send "$ip: Kurulum Tamamlandı . Plot üretimine Başlandı ."
 screen -S spaceplot ./build/chia_plot -n 10000 -r 24 -u 256 -d /root/disk1/data/ -2 /root/ram/ -t  /root/disk1/temp/ -c xch1j87jt0uk2p79g7v8pf5ewyxuh0hrzxkxu6rqqg9zzcn3wamjdrxq5sxtve -f 88fe767ffb45d152457a4f6deeffc75cc885bc041a7215a51906261e56680724c8dc878772c1ea4aa60644e2aec0ac97  
