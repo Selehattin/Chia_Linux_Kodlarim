48f7b85e7c1d74bc158ebfdf4da9ff00ff954c8f
Selehattin <32058059+Selehattin@users.noreply.github.com>
2022-11-13 00:46:47 +0300

    Update otoTasima.sh

--- otoTasima.sh
+++ otoTasima.sh
@@ -1,18 +1,18 @@
 ip=$(curl ifconfig.me)
 while :
 do
-x=($(ls -lR /root/disk2/* | wc -l))
+x=($(ls -lR /root/disk1/data/* | wc -l))
 y=($(ls -lR /root/disk2/* | wc -l))
 
-if [ $((y%0)) == 0 ];
+if [ $x == 0 ];
 then
 telegram-send "Plot Klsor boş  .. :) $ip"
 sleep 3600
 fi
 
-if [ $((x%4)) == 0 ];
+if [ $y == 0 ];
  then
-  ls -Q disk1 | head -4 | xargs -i mv disk1/{} disk2/
+  ls -Q /root/disk1/data | head -4 | xargs -i mv /root/disk1/data/{} disk2/
   telegram-send "Yeni Plotlar Tasindi .. :) $ip"
 fi
 sleep 5
