4a6a442a1d416c21d17446d5ead6ffeee5929c89
Selahattin YORA <32058059+Selehattin@users.noreply.github.com>
2023-05-18 17:19:10 +0300

    Update Onemli_Linux_Notlari.sh

--- Onemli_Linux_Notlari.sh
+++ Onemli_Linux_Notlari.sh
@@ -2,3 +2,8 @@
 python multifactory.py --quick-setup -1
 
 python masshare.py -d 0AGCWHUcmHkCJUk9PVA
+
+for /l %%x in (100, 1,150) do (
+ rclone copy "aktif:/YEDEK_01/" "pasif:/YEDEK_03/" --ignore-existing --transfers 6 --drive-service-account-file=C:\Rclone\accounts\duz\%%x.json -P -v -c   --drive-stop-on-upload-limit  --max-transfer 608G
+)
+pause
