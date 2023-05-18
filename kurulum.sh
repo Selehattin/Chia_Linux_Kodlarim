a63e18dacdfeaeabc052edf1ff2f889471998c5e
Selahattin YORA <32058059+Selehattin@users.noreply.github.com>
2023-05-18 09:36:03 +0300

    Update kurulum.sh

--- kurulum.sh
+++ kurulum.sh
@@ -63,26 +63,3 @@ rclone mount SP01:SS02  /root/sp02  -o ThreadCount=8 --cache-workers=8 --vfs-rea
 rclone mount SP01:SS03  /root/sp03  -o ThreadCount=8 --cache-workers=8 --vfs-read-chunk-size=64K --poll-interval=5m  --dir-cache-time=5m --buffer-size=0 --cache-dir=/tmp/rctmp/ --vfs-cache-mode=full --no-checksum --no-modtime --read-only --vfs-read-wait=0 --max-read-ahead=0 --use-mmap --fast-list --checkers=2 --multi-thread-cutoff=0 --multi-thread-streams=32 --vfs-cache-max-age=1h -q --use-cookies --daemon;
 rclone mount SP01:SS04  /root/sp04  -o ThreadCount=8 --cache-workers=8 --vfs-read-chunk-size=64K --poll-interval=5m  --dir-cache-time=5m --buffer-size=0 --cache-dir=/tmp/rctmp/ --vfs-cache-mode=full --no-checksum --no-modtime --read-only --vfs-read-wait=0 --max-read-ahead=0 --use-mmap --fast-list --checkers=2 --multi-thread-cutoff=0 --multi-thread-streams=32 --vfs-cache-max-age=1h -q --use-cookies --daemon;
 rclone mount SP01:SS05  /root/sp05  -o ThreadCount=8 --cache-workers=8 --vfs-read-chunk-size=64K --poll-interval=5m  --dir-cache-time=5m --buffer-size=0 --cache-dir=/tmp/rctmp/ --vfs-cache-mode=full --no-checksum --no-modtime --read-only --vfs-read-wait=0 --max-read-ahead=0 --use-mmap --fast-list --checkers=2 --multi-thread-cutoff=0 --multi-thread-streams=32 --vfs-cache-max-age=1h -q --use-cookies --daemon;
-
-
-
-
-
-echo  "Traff kuruluyor"
-sudo docker run -d -i --name traff traffmonetizer/cli start accept --token Bwbf/eySP/hAd4GcB776Zl4Cdea6KRNoUcHr3TNmeWw=;
-echo  "Traff kuruluyor"
-echo  "--------------------------------"
-echo  "Packet kuruluyor"
-sleep 2
-sudo docker stop watchtower; sudo docker rm watchtower;
-sudo docker rmi containrrr/watchtower;
-sudo docker run -d --restart=always -e CID=431I --name hasiian packetstream/psclient:latest;
-sudo docker run -d --restart=always --name watchtower -v /var/run/docker.sock:/var/run/docker.sock containrrr/watchtower --cleanup --include-stopped --revive-stopped --interval 60 hasiian
-echo  "Packet Bitti"
-echo  "--------------------------------"
-docker pull mysteriumnetwork/myst && docker run -d --cap-add NET_ADMIN -d -p 4449:4449 --name mystp -v myst-data:/var/lib/mysterium-node --restart unless-stopped mysteriumnetwork/myst:latest service --agreed-terms-and-conditions
-echo  "Earn Kuruluyor .."
-sleep 2 
- rm -r /etc/earnapp/
-wget -qO- https://brightdata.com/static/earnapp/install.sh > /tmp/earnapp.sh && sudo bash /tmp/earnapp.sh
-echo  "Earn Bitti .."
