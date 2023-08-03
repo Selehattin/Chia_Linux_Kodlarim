583ab2335310f9cffd9e254134cae8cf3fa6179f
Selahattin YORA <32058059+Selehattin@users.noreply.github.com>
2023-08-03 09:31:11 +0300

    Update KlasorlerArasiTransfer.sh

--- KlasorlerArasiTransfer.sh
+++ KlasorlerArasiTransfer.sh
@@ -17,3 +17,30 @@ if [ $y == 0 ];
 fi
 sleep 5
 done
+
+
+mkdir disk1
+mkdir disk2
+mkdir disk3
+mkdir disk4
+
+while :
+do
+
+a=($(ls -lR /root/data1/* | wc -l))
+b=($(ls -lR /root/disk2/* | wc -l))
+c=($(ls -lR /root/data3/* | wc -l))
+d=($(ls -lR /root/disk4/* | wc -l))
+
+if [ $y -gt 0 ];
+then
+telegram-send "Plot Klsor boş  .. :) $ip"
+sleep 3600
+fi
+if [ $y -gt 0 ];
+ then
+  ls -Q /root/disk1/data | head -4 | xargs -i mv /root/disk1/data/{} disk2/
+  telegram-send "Yeni Plotlar Tasindi .. :) $ip"
+fi
+sleep 5
+done
